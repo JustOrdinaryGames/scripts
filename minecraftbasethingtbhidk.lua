60a4a778d884686aa9d92b1ef53ab185970ad83c
JustOrdinaryGames <65363269+JustOrdinaryGames@users.noreply.github.com>
2022-06-18 11:58:29 +0000

    Add files via upload

--- minecraftbasethingtbhidk.lua
+++ minecraftbasethingtbhidk.lua
@@ -1,5 +1,6 @@
 local lib = {}
 
+local savedImages = {}
 local pastDraws = {}
 local drawScreenFunc = nil
 local mouseClickedFunc = nil
@@ -23,14 +24,76 @@ local drawScreen = game.RunService.RenderStepped:Connect(function()
     pastDraws = {}
     if drawScreenFunc ~= nil then
         local args = {}
-        function args.drawRect(x,y,width,height,color)
+        function args.drawRect(x,y,width,height,color,transparency)
             local square = Drawing.new("Square")
             square.Position = Vector2.new(x,y)
             square.Size = Vector2.new(width,height)
             square.Visible = true
             square.Transparency = 1
             square.Color = color
+            if transparency ~= nil then
+                square.Transparency = 1 - transparency -- Converts bullshit transparency to not bullshit transparency
+            end
             table.insert(pastDraws, square)
+        end
+        function args.drawImage(x,y,width,height,color,imageurl,transparency)
+            if savedImages[imageurl] == nil then
+                savedImages[imageurl] = game:HttpGet(imageurl)
+            end
+            local square = Drawing.new("Image")
+            square.Position = Vector2.new(x,y)
+            square.Size = Vector2.new(width,height)
+            square.Visible = true
+            square.Transparency = 1
+            square.Color = color
+            square.Data = savedImages[imageurl]
+            if transparency ~= nil then
+                square.Transparency = 1 - transparency -- Converts bullshit transparency to not bullshit transparency
+            end
+            table.insert(pastDraws, square)
+        end
+        function args.drawString(msg,x,y,size,color,transparency)
+            local text = Drawing.new("Text")
+            text.Position = Vector2.new(x,y)
+            text.Size = size
+            text.Visible = true
+            text.Transparency = 1
+            text.Color = color
+            text.Text = msg
+            text.Font = 2
+            if transparency ~= nil then
+                text.Transparency = 1 - transparency -- Converts bullshit transparency to not bullshit transparency
+            end
+            table.insert(pastDraws, text)
+        end
+        function args.drawStringWithOutline(msg,x,y,size,color,outline,transparency)
+            local text = Drawing.new("Text")
+            text.Position = Vector2.new(x,y)
+            text.Size = size
+            text.Visible = true
+            text.Transparency = 1
+            text.Color = color
+            text.Text = msg
+            text.Outline = true
+            text.OutlineColor = outline
+            text.Font = 2
+            if transparency ~= nil then
+                text.Transparency = 1 - transparency -- Converts bullshit transparency to not bullshit transparency
+            end
+            table.insert(pastDraws, text)
+        end
+        function args.getStringWidth(msg, size)
+            local text = Drawing.new("Text")
+            text.Position = Vector2.new(-100000,-100000)
+            text.Size = size
+            text.Visible = true
+            text.Transparency = 1
+            text.Color = Color3.new(1,1,1)
+            text.Text = msg
+            text.Font = 2
+            local bounds = text.TextBounds
+            text:Remove()
+            return bounds.X
         end
         drawScreenFunc(args, mouse.X, mouse.Y)
     end
