a01094211a116f811d6fa23de64fc87d91b946fa
JustOrdinaryGames <65363269+JustOrdinaryGames@users.noreply.github.com>
2022-06-20 11:09:26 +0000

    add getFontHeight

--- minecraftbasethingtbhidk.lua
+++ minecraftbasethingtbhidk.lua
@@ -95,6 +95,19 @@ local drawScreen = game.RunService.RenderStepped:Connect(function()
             text:Remove()
             return bounds.X
         end
+        function args.getFontHeight(size)
+            local text = Drawing.new("Text")
+            text.Position = Vector2.new(-100000,-100000)
+            text.Size = size
+            text.Visible = true
+            text.Transparency = 1
+            text.Color = Color3.new(1,1,1)
+            text.Text = 'test'
+            text.Font = 3
+            local bounds = text.TextBounds
+            text:Remove()
+            return bounds.Y
+        end
         drawScreenFunc(args, mouse.X, mouse.Y + 36)
     end
 end)
